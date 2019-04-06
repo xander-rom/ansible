8e69640af2ead7850484269e55259be796f5e96a
xander-rom <alexander.romanovi4@gmail.com>
2019-04-07 02:42:40 +0300

    modified module

--- jenkins cluster/library/deploy.sh
+++ jenkins cluster/library/deploy.sh
@@ -5,24 +5,24 @@
 # tom_user=deployer
 # tom_pass=s3cret
 # dep_path=aramanovich
-# build=54
+# build=50
 # stable=56
-# act=rollback
+# act=deploy
 #### functions
 function deploy {
 	if [[ $tom_url ]] && [[ $dep_path ]] && [[ $tom_user ]] && [[ $tom_pass ]] && [[ $build ]]
 	then
-		curl -IL $tom_url$dep_path > cur_page
-		dep_res=$(curl -T 'tmp/helloworld-ws/target/helloworld-ws.war'\
-  				"http://$tom_user:$tom_pass@$tom_url/manager/text/deploy?path=/$dep_path&tag=$build&update=true")
-		if [[ "$dep_res" == *FAIL* ]]
+		curl -s -IL $tom_url$dep_path > cur_page
+		if [[ -z "tmp/helloworld-ws/target/helloworld-ws.war" ]]; then touch fuck; fi
+		curl -s --upload-file "tmp/helloworld-ws/target/helloworld-ws.war" "http://$tom_user:$tom_pass@$tom_url/manager/text/deploy?path=/$dep_path&tag=$build&update=true" > dep_res
+		if [[ $(cat dep_res) == *FAIL* ]] || [[ $(cat dep_res) == '' ]]
 		then res="Deploy failed with message: $dep_res"
-		elif [[ "$dep_res" == *OK* ]] && [[ $(cat cur_page) == *404* ]]
+		elif [[ $(cat dep_res) == *OK* ]] && [[ $(cat cur_page) == *404* ]]
 		then res="Deploy is succesful. Actual page is $tomcat_url$dep_path. Actual build is $build"
 		else
-			cat cur_page | grep Last-Modified > cur_page
-			curl -IL $tom_url$dep_path | grep Last-Modified > new_page
-			if [[ $(cat cur_page) == $(cat new_page) ]]
+			cat cur_page | grep Last-Modified > cur_time
+			curl -s -IL $tom_url$dep_path | grep Last-Modified > new_time
+			if [[ $(cat cur_time) == $(cat new_time) ]]
 				then res="Deploy failed or you deployed the same app"
 				else res="Deploy is succesful. Actual page is $tomcat_url$dep_path. Actual build is $build"
 			fi
@@ -33,7 +33,7 @@ function deploy {
 function rollback {
 	if [[ $tom_url ]] && [[ $dep_path ]] && [[ $tom_user ]] && [[ $tom_url ]] && [[ $tom_pass ]] && [[ $stable ]]
 	then
-		curl "http://$tom_user:$tom_pass@$tom_url/manager/text/deploy?path=/$dep_path&update=true&tag=$stable" > roll
+		curl "http://$tom_user:$tom_pass@$tom_urlmanager/text/deploy?path=/$dep_path&update=true&tag=$stable" > roll
 		if [[ $(cat roll) == *OK* ]]
 			then res="Rollback is successful. Actual page is $tomcat_url$dep_path. Actual build is $stable"
 			else res="Rollback failed"
@@ -50,9 +50,10 @@ url_ready=$(echo $tom_url | sed 's|http://||') && tom_url=$url_ready
 case $act in
 	deploy)
 	 ######## get the artifact from nexus
-		mkdir ./tmp
+		if ! [[ -d tmp ]]; then mkdir ./tmp
+		fi
 		nexus_url="http://nexus-ci.playpit.by/repository/MNT-pipeline-training/aramanovich/pipeline-aramanovich/$build/pipeline-aramanovich-$build.tar.gz"
-		wget $nexus_url 
+		wget -q $nexus_url 
 		tar -zxf pipeline-aramanovich-$build.tar.gz -C tmp
  		deploy
   	;;
